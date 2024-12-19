3a842400b55344a5a9b2a1b17fde0e4eaa256a5a
Antherreli <antherrely@gmail.com>
2024-12-19 17:26:13 -0300

    incial commit

---
+++ versionamento.c
@@ -0,0 +1,6 @@
+// commit --> salva mudanças feitas no repositório local
+// push --> envia mudanças para repositório remoto
+// pull --> busca mudanças do repositório remoto
+// git merge --> unir modificaçãoes ao branch principal
+// git clone --> cria repositório a partir de um existente
+// git init --> cria repositório novo 
